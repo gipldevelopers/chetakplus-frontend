ce6effe83773021f846f80b22b808eaa657b0747
Panchal Urvisha <urvisha@gohilinfotech.com>
2026-04-06 14:48:26 +0530

    Create adminAuth.js

---
+++ src/lib/adminAuth.js
@@ -0,0 +1,25 @@
+﻿const ADMIN_SESSION_KEY = "cp_admin_session";
+
+export const isAdminAuthenticated = () => {
+  if (typeof window === "undefined") {
+    return false;
+  }
+
+  return window.localStorage.getItem(ADMIN_SESSION_KEY) === "active";
+};
+
+export const setAdminSession = () => {
+  if (typeof window === "undefined") {
+    return;
+  }
+
+  window.localStorage.setItem(ADMIN_SESSION_KEY, "active");
+};
+
+export const clearAdminSession = () => {
+  if (typeof window === "undefined") {
+    return;
+  }
+
+  window.localStorage.removeItem(ADMIN_SESSION_KEY);
+};
